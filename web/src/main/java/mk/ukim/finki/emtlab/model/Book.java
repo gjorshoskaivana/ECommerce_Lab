0a504a05ae8d15ff73e5a956efceda44e5f778d7
gjorshoskaivana <63347774+gjorshoskaivana@users.noreply.github.com>
2021-04-14 13:36:51 +0200

    init

---
+++ web/src/main/java/mk/ukim/finki/emtlab/model/Book.java
@@ -0,0 +1,34 @@
+package mk.ukim.finki.emtlab.model;
+
+import lombok.Data;
+import mk.ukim.finki.emtlab.model.enumerations.Category;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Enumerated(value = EnumType.STRING)
+    private Category category;
+
+    @ManyToOne
+    private Author author;
+
+    private Integer availableCopies;
+
+    public Book(String name, Category category, Author author, Integer availableCopies) {
+        this.name = name;
+        this.category = category;
+        this.author = author;
+        this.availableCopies = availableCopies;
+    }
+
+    public Book(){}
+}
